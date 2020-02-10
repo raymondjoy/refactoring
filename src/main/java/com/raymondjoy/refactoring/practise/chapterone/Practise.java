f0c4bb9e8c0f48b4e2754d3e228485864826ede5
hong.chen <hong.chen@thoughtworks.com>
2020-02-10 13:16:31 +0800

    [ChenHong] #N/A fix: add method amountFor

--- src/main/java/com/raymondjoy/refactoring/practise/chapterone/Practise.java
+++ src/main/java/com/raymondjoy/refactoring/practise/chapterone/Practise.java
@@ -36,23 +36,7 @@ public class Practise {
         for (Invoices perf : (List<Invoices>) invoicesMap.get("performances")) {
             Plays play = playsMap.get(perf.getPlayID());
             int thisAmount = 0;
-            switch (play.getType()) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.getAudience() > 30) {
-                        thisAmount += 1000 * (perf.getAudience() - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (perf.getAudience() > 20) {
-                        thisAmount += 10000 + 500 * (perf.getAudience() - 20);
-                    }
-                    thisAmount += 300 * perf.getAudience();
-                    break;
-                default:
-                    throw new Error("unknown type");
-            }
+            thisAmount = amountFor(perf, play);
             volumeCredits += Math.max(perf.getAudience() - 30, 0);
             if ("comedy".equals(play.getType())) volumeCredits += Math.floor(perf.getAudience() / 5);
             result += "  " + play.getName() + ": " + thisAmount / 100 + "￥(" + perf.getAudience() + " seats)\n";
@@ -61,5 +45,27 @@ public class Practise {
         result += "Amount owed is " + totalAmount / 100 + "￥ \n";
         result += "You earned " + volumeCredits + " credits\n";
         return result;
+    }
+
+    private static int amountFor(Invoices perf, Plays play) {
+        int thisAmount;
+        switch (play.getType()) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.getAudience() > 30) {
+                    thisAmount += 1000 * (perf.getAudience() - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.getAudience() > 20) {
+                    thisAmount += 10000 + 500 * (perf.getAudience() - 20);
+                }
+                thisAmount += 300 * perf.getAudience();
+                break;
+            default:
+                throw new Error("unknown type");
+        }
+        return thisAmount;
     }
 }
